be4c795365f7e57bb0b8039d128be4e1850eebf7
calaw <calaw1110@gmail.com>
2021-03-09 23:07:19 +0800

    新增三個component 當作分頁 練習呈現各種結構句、學習呈現用

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'sandox';
+  currentPage: string = '';
+  btn_id: string='';
+  constructor() { }
+  ngOnInit() {
+  }
+  ngAfterViewInit() {
+
+  }
+  btnClick(event: any) {
+    console.log(event);
+    let id = event.target.id;
+    this.btn_id = id;
+    switch (id) {
+      case 'btn-ngFor':
+        this.currentPage = 'page-ngFor';
+        break;
+      case 'btn-ngIf':
+        this.currentPage = 'page-ngIf';
+        break;
+      case 'btn-ngSwitch':
+        this.currentPage = 'page-ngSwitch';
+        break;
+    }
+  }
 }
